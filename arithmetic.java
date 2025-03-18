d8dd3639250df30de1eab35650643fc32abcd6a6
bhargav-eng <245122748064@mvsrec.edu.in>
2025-03-18 12:07:21 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SimpleArithmetic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking two numbers as input
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Performing arithmetic operations
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        // Displaying the results
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+
+        scanner.close();
+    }
+}
+
